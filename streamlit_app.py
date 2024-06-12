067a1759e38c4acf1be10672a442b717352f50e3
2303430205 <1651243224@qq.com>
2024-06-12 15:11:50 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+#import mymodel as m
+
+st.write('''
+# Sales model
+Below are our sales predictions
+for this customer.
+''')
+
+'''window = st.slider("Forecast window")
+st.write(m.run(window=window))'''
